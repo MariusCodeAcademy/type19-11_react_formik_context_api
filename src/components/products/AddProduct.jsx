b870feafee6a5d135363f28511da2d751b5ee515
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-24 09:38:24 +0200

    form

--- src/components/products/AddProduct.jsx
+++ src/components/products/AddProduct.jsx
@@ -16,14 +16,46 @@ export default function AddProduct() {
       <h2 className='text-4xl my-5'>Create new product</h2>
 
       <form>
-        <label className='block'>
+        <label className='block mb-4'>
           <span className='text-lg block'>Title</span>
           <input
-            className='border w-full px-2 py-1 border-slate-500 rounded-md '
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
             type='text'
             placeholder='Enter Title'
           />
         </label>
+        <label className='block mb-4'>
+          <span className='text-lg block'>Price</span>
+          <input
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
+            type='number'
+            step={0.01}
+            placeholder='Enter Price'
+          />
+        </label>
+        <label className='block mb-4'>
+          <span className='text-lg block'>Rating</span>
+          <input
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
+            type='number'
+            step={0.1}
+            placeholder='Enter Rating'
+          />
+        </label>
+        <label className='block mb-4'>
+          <span className='text-lg block'>Stock</span>
+          <input
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
+            type='number'
+            step={1}
+            placeholder='Enter Stock'
+          />
+        </label>
+        <button
+          className='mt-6 text-lg border px-6 py-2 border-slate-600 rounded-md hover:bg-indigo-600 hover:text-white transition-colors '
+          type='submit'>
+          Add
+        </button>
       </form>
     </div>
   );
