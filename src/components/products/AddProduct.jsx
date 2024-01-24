6bc24d89d919da623a504c015602a2b2e722128c
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-24 10:28:54 +0200

    form

--- src/components/products/AddProduct.jsx
+++ src/components/products/AddProduct.jsx
@@ -20,21 +20,28 @@ export default function AddProduct() {
       price: '',
       rating: '',
       stock: '',
+      description: '',
+      isOnSale: false,
+      brand: '',
+      category: '',
+      thumbnail: '',
+      adminEmail: '',
     },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
+      // fetch
     },
   });
 
   console.log('formik.values ===', formik.values);
 
   return (
-    <div className='container '>
+    <div className='container mb-96'>
       <h2 className='text-4xl my-5'>Create new product</h2>
       <div>
         <p>Title: {formik.values.title}</p>
       </div>
-      <form onSubmit={formik.handleSubmit}>
+      <form className='grid grid-cols-2 gap-x-5' onSubmit={formik.handleSubmit}>
         <label className='block mb-4'>
           <span className='text-lg block'>Title</span>
           <input
@@ -83,8 +90,63 @@ export default function AddProduct() {
             placeholder='Enter Stock'
           />
         </label>
+
+        <label className='block mb-4'>
+          <span className='text-lg block'>Brand</span>
+          <input
+            onChange={formik.handleChange}
+            value={formik.values.brand}
+            name='brand'
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
+            type='text'
+            placeholder='Enter Brand'
+          />
+        </label>
+        <label className='block mb-4'>
+          <span className='text-lg block'>Category</span>
+          <input
+            onChange={formik.handleChange}
+            value={formik.values.category}
+            name='category'
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
+            type='text'
+            placeholder='Enter Category'
+          />
+        </label>
+        <label className='block mb-4 '>
+          <span className='text-lg block'>Thumbnail</span>
+          <input
+            onChange={formik.handleChange}
+            value={formik.values.thumbnail}
+            name='thumbnail'
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
+            type='text'
+            placeholder='Enter Thumbnail'
+          />
+        </label>
+        <label className='block mb-4'>
+          <span className='text-lg block'>Admin Email</span>
+          <input
+            onChange={formik.handleChange}
+            value={formik.values.adminEmail}
+            name='adminEmail'
+            className='border w-full px-3 py-[6px] border-slate-300 rounded-md '
+            type='email'
+            placeholder='Enter Admin Email'
+          />
+        </label>
+        <label className='block mb-4 col-span-2'>
+          <span className='text-lg block'>Description</span>
+          <textarea
+            onChange={formik.handleChange}
+            value={formik.values.description}
+            name='description'
+            className='border min-h-20 w-full px-3 py-[6px] border-slate-300 rounded-md '
+            placeholder='Enter Description'
+          />
+        </label>
         <button
-          className='mt-6 text-lg border px-6 py-2 border-slate-600 rounded-md hover:bg-indigo-600 hover:text-white transition-colors '
+          className='mt-6 place-self-start text-lg border px-6 py-2 border-slate-600 rounded-md hover:bg-indigo-600 hover:text-white transition-colors '
           type='submit'>
           Add
         </button>
