e609df1a0e8ed037d3f722999e48a6066b3ecec4
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-24 09:17:36 +0200

    producs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import AddProduct from './components/products/AddProduct';
+
 export default function App() {
   return (
     <div className='App container mx-auto'>
-      <h1 className='text-3xl font-bold underline'>Hello, world!</h1>
-      <button className='px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>
-        Nice button
-      </button>
-      <h2>Hello, world!</h2>
-      <p>Hello, world!</p>
+      <h1 className='text-3xl font-bold underline'>Formik</h1>
       <hr />
+      <AddProduct />
     </div>
   );
 }
---
+++ src/components/products/AddProduct.jsx
@@ -0,0 +1,15 @@
+const product = {
+  title: 'iPhone 9',
+  description: 'An apple mobile which is nothing like apple',
+  price: 549,
+  isOnSale: true,
+  rating: 4.69,
+  stock: 94,
+  brand: 'Apple',
+  category: 'smartphones',
+  thumbnail: '...',
+};
+
+export default function AddProduct() {
+  return <div>AddProduct</div>;
+}
