4bfc78436e12de5f91289925cf456778e3355af0
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-24 09:29:19 +0200

    input

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import AddProduct from './components/products/AddProduct';
 
 export default function App() {
   return (
-    <div className='App container mx-auto'>
-      <h1 className='text-3xl font-bold underline'>Formik</h1>
+    <div className=''>
+      <div className='container '>
+        <h1 className='text-3xl font-bold underline'>Formik</h1>
+      </div>
       <hr />
       <AddProduct />
     </div>
--- src/components/products/AddProduct.jsx
+++ src/components/products/AddProduct.jsx
@@ -11,5 +11,20 @@ const product = {
 };
 
 export default function AddProduct() {
-  return <div>AddProduct</div>;
+  return (
+    <div className='container '>
+      <h2 className='text-4xl my-5'>Create new product</h2>
+
+      <form>
+        <label className='block'>
+          <span className='text-lg block'>Title</span>
+          <input
+            className='border w-full px-2 py-1 border-slate-500 rounded-md '
+            type='text'
+            placeholder='Enter Title'
+          />
+        </label>
+      </form>
+    </div>
+  );
 }
