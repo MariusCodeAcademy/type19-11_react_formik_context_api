f047c4aeaebf1b5836497574bc1d2e4e92c1306e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-25 11:05:15 +0200

    UnAuthorizedPage

---
+++ src/pages/UnAuthorizedPage.jsx
@@ -0,0 +1,8 @@
+export default function UnAuthorizedPage() {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl mb-5'>Only for registered users</h1>
+      <p>Please login to see the page</p>
+    </div>
+  );
+}
