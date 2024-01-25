70851c4c23a0087ccf903191b033af933f1c8ae3
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-25 10:58:39 +0200

    pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import AddProduct from './components/products/AddProduct';
 import ProductsList from './components/products/ProductsList';
 import Header from './components/layout/Header';
 import { useState } from 'react';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+import ProductPage from './pages/ProductPage';
+import AddProductPage from './pages/AddProductPage';
 
 export default function App() {
   const [token, setToken] = useState('');
@@ -20,9 +24,10 @@ export default function App() {
     <div className=''>
       <Header isUserLoggedIn={isUserLoggedIn} />
       <Routes>
-        <Route path='/login' element={<Login onLogin={handleLogin} />} />
-        <Route path='/products' element={<ProductsList />} />
-        <Route path='/products/add' element={<AddProduct />} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/auth/login' element={<AuthPage onLogin={handleLogin} />} />
+        <Route path='/products' element={<ProductPage />} />
+        <Route path='/products/add' element={<AddProductPage />} />
       </Routes>
     </div>
   );
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,13 +1,11 @@
-export default function AddPostPage() {
+export default function ProductPage() {
   return (
     <div className='container'>
-      <h1 className='text-3xl mb-5'>AddPostPage</h1>
+      <h1 className='text-3xl mb-5'>ProductPage</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quidem nam fugit dolor
         dolorem iure voluptatum optio quod, laboriosam, molestiae illum, beatae distinctio corporis
         cumque placeat laudantium accusantium aspernatur consequatur exercitationem labore. Adipisci
-        ad tempore eaque obcaecati facilis debitis dicta laboriosam cupiditate reiciendis quibusdam
-        deserunt dolores temporibus, officia aliquid error!
       </p>
     </div>
   );
