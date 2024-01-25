3137721ce836f69f79215b182ca9c4aa0ffa3af8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-25 10:56:09 +0200

    pages

---
+++ src/pages/AddPostPage.jsx
@@ -0,0 +1,14 @@
+export default function AddPostPage() {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl mb-5'>AddPostPage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quidem nam fugit dolor
+        dolorem iure voluptatum optio quod, laboriosam, molestiae illum, beatae distinctio corporis
+        cumque placeat laudantium accusantium aspernatur consequatur exercitationem labore. Adipisci
+        ad tempore eaque obcaecati facilis debitis dicta laboriosam cupiditate reiciendis quibusdam
+        deserunt dolores temporibus, officia aliquid error!
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/PostPage.jsx
@@ -0,0 +1,12 @@
+export default function PostPage() {
+  return (
+    <div className='container'>
+      <h1 className='text-3xl mb-5'>PostPage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quidem nam fugit dolor
+        dolorem iure voluptatum optio quod, laboriosam, molestiae illum, beatae distinctio corporis
+        cumque placeat laudantium accusantium aspernatur consequatur exercitationem labore. Adipisci
+      </p>
+    </div>
+  );
+}
