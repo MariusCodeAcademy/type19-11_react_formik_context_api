122dff288f1c50ebe4a90c17f230e0b8bc79cab4
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-26 10:40:56 +0200

    create context

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import Login from './components/auth/Login';
-import AddProduct from './components/products/AddProduct';
-import ProductsList from './components/products/ProductsList';
 import Header from './components/layout/Header';
 import { useState } from 'react';
 import HomePage from './pages/HomePage';
@@ -10,6 +7,7 @@ import ProductPage from './pages/ProductPage';
 import AddProductPage from './pages/AddProductPage';
 import UnAuthorizedPage from './pages/UnAuthorizedPage';
 import SingleProductPage from './pages/SingleProductPage';
+import AuthContext from './store/AuthContex';
 
 export default function App() {
   const tokenFromStorage = localStorage.getItem('userToken');
@@ -29,26 +27,31 @@ export default function App() {
 
   const isUserLoggedIn = Boolean(token);
   console.log('isUserLoggedIn ===', isUserLoggedIn);
+
+  const ctxValue = 500;
+
   return (
-    <div className=''>
-      <Header isUserLoggedIn={isUserLoggedIn} onLogout={handleLogout} />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route
-          path='/auth/login'
-          element={
-            !isUserLoggedIn ? <AuthPage onLogin={handleLogin} /> : <Navigate to={'/products'} />
-          }
-        />
-        <Route path='/products' element={<ProductPage />} />
-        {/* protected route */}
-        <Route
-          path='/products/add'
-          element={isUserLoggedIn ? <AddProductPage /> : <Navigate to={'/unauthorized'} />}
-        />
-        <Route path='/products/:prodId/' element={<SingleProductPage />} />
-        <Route path='/unauthorized' element={<UnAuthorizedPage />} />
-      </Routes>
-    </div>
+    <AuthContext.Provider value={ctxValue}>
+      <div className=''>
+        <Header isUserLoggedIn={isUserLoggedIn} onLogout={handleLogout} />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route
+            path='/auth/login'
+            element={
+              !isUserLoggedIn ? <AuthPage onLogin={handleLogin} /> : <Navigate to={'/products'} />
+            }
+          />
+          <Route path='/products' element={<ProductPage />} />
+          {/* protected route */}
+          <Route
+            path='/products/add'
+            element={isUserLoggedIn ? <AddProductPage /> : <Navigate to={'/unauthorized'} />}
+          />
+          <Route path='/products/:prodId/' element={<SingleProductPage />} />
+          <Route path='/unauthorized' element={<UnAuthorizedPage />} />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 }
